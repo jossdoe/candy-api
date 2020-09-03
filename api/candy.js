0b3b6d714f4b78e4079389e18b2b16b98a51f42a
Joss Doebler <mail@joss-doebler.de>
2020-09-03 18:23:08 +0200

    Removing comments

--- api/candy.js
+++ api/candy.js
@@ -5,10 +5,4 @@ const api = express.Router();
 
 api.route('/').get(getAllCandy);
 
-// api
-//   .route('/:id')
-//   .get(getUser)
-//   .put(updateUser)
-//   .delete(deleteUser)
-
 module.exports = api;
