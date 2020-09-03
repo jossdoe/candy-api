d96b7905813aeb1fa8e0e6ddb3441b3da0f0e55c
Joss Doebler <mail@joss-doebler.de>
2020-09-03 21:35:36 +0200

    Add different filtering methods for the data

--- api/candy.js
+++ api/candy.js
@@ -1,8 +1,16 @@
 const express = require('express');
-const { getAllCandy } = require('../controllers/candy');
+const {
+  getAllCandy,
+  getCandyById,
+  getCandyByName,
+  getCandyByCategory,
+} = require('../controllers/candy');
 
 const api = express.Router();
 
 api.route('/').get(getAllCandy);
+api.route('/id/:id').get(getCandyById);
+api.route('/name/:name').get(getCandyByName);
+api.route('/:cat').get(getCandyByCategory);
 
 module.exports = api;
--- controllers/candy.js
+++ controllers/candy.js
@@ -17,6 +17,66 @@ const getAllCandy = async (req, res, next) => {
   }
 };
 
+const getCandyById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const candy = await Candy.find({ _id: id });
+    const cleanCandy = JSON.parse(JSON.stringify(candy));
+
+    res.json({
+      success: true,
+      msg: `show candy with id ${id}`,
+      data: cleanCandy.map(({ winpercent, ...properties }) => ({
+        ...properties,
+      })),
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const getCandyByName = async (req, res, next) => {
+  try {
+    const { name } = req.params;
+
+    const candy = await Candy.find({ name: { $regex: name, $options: 'i' } });
+    const cleanCandy = JSON.parse(JSON.stringify(candy));
+
+    res.json({
+      success: true,
+      msg: `show candy where name contains ${name}`,
+      data: cleanCandy.map(({ winpercent, ...properties }) => ({
+        ...properties,
+      })),
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const getCandyByCategory = async (req, res, next) => {
+  try {
+    const { cat } = req.params;
+
+    const candy = await Candy.find({ [cat]: 1 });
+    const cleanCandy = JSON.parse(JSON.stringify(candy));
+
+    res.json({
+      success: true,
+      msg: `show candy that fits category ${cat}`,
+      data: cleanCandy.map(({ winpercent, ...properties }) => ({
+        ...properties,
+      })),
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
 module.exports = {
   getAllCandy,
+  getCandyById,
+  getCandyByName,
+  getCandyByCategory,
 };
