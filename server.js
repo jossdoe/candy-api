17cfb72e3ed2f24bb3e1a9c608bdf65c90e11cf1
Joss Doebler <mail@joss-doebler.de>
2020-09-03 18:19:33 +0200

    Fix PORT in server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const candy = require('./api/candy');
 const errorHandler = require('./middleware/error');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 connectDB();
 
